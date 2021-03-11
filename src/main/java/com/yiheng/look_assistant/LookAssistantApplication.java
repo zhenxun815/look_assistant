d8836ed9b8c0285b6c4273e6ff0892bb19d1c6d1
zhenxun815 <guoyiheng89@gmail.com>
2021-03-12 05:49:50 +0800

    jpa basic

--- src/main/java/com/yiheng/look_assistant/LookAssistantApplication.java
+++ src/main/java/com/yiheng/look_assistant/LookAssistantApplication.java
@@ -1,9 +1,12 @@
 package com.yiheng.look_assistant;
 
+import com.yiheng.look_assistant.db.repository.BaseRepository;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Yiheng
@@ -12,6 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(repositoryBaseClass = BaseRepository.class)
 public class LookAssistantApplication extends Application {
 
     public static void main(String[] args) {
--- src/main/java/com/yiheng/look_assistant/db/repository/BaseRepository.java
+++ src/main/java/com/yiheng/look_assistant/db/repository/BaseRepository.java
@@ -11,14 +11,12 @@ import org.springframework.data.repository.NoRepositoryBean;
  * 逻辑删除，重写删除方法。
  */
 @NoRepositoryBean
-public interface BaseRepository extends JpaRepository<BaseModel, Long> {
+public interface BaseRepository<T extends BaseModel> extends JpaRepository<T, Long> {
 
-
-    @Override
-    default void deleteById(Long id) {
-        BaseModel model = getOne(id);
-        model.setDelFlag(1);
-        save(model);
+    default void logicDeleteById(Long id) {
+        T t = getOne(id);
+        t.setDelFlag(1);
+        save(t);
     }
 
 }
