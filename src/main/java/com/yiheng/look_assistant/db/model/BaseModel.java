6cd6ca75d09995439bb8cdad50d67054f34a6c9b
zhenxun815 <guoyiheng89@gmail.com>
2021-03-12 02:13:35 +0800

    add BaseModel.java

---
+++ src/main/java/com/yiheng/look_assistant/db/model/BaseModel.java
@@ -0,0 +1,47 @@
+package com.yiheng.look_assistant.db.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author Yiheng
+ * @since 2021/3/12 上午12:31
+ */
+@Slf4j
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createTime", "updateTime"}, allowGetters = true)
+public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GenericGenerator(name = "jpa-uuid", strategy = "uuid")
+    //@GeneratedValue(generator = "jpa-uuid")
+    @JsonInclude
+    protected String id;
+
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    protected Date createTime;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    protected Date updateTime;
+}
